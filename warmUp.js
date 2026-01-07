15c87e90cedf8d70186746e96ea33e0c64488508
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2026-01-08 07:20:40 +0800

    lvl 2 done

--- warmUp.js
+++ warmUp.js
@@ -64,3 +64,50 @@ function countUp(n) {
 }
 
 console.log(countUp(5));
+
+// Level 2: Logic Re-activation
+// Return vs. Log
+console.log(`---------Lvl 2---------`)
+
+function double(n) {
+    return n * 2
+}
+
+console.log(double(2));
+
+// Count even numbers
+let arr = [1, 2, 3, 4, 5, 6];
+
+function countEven(arr) {
+    // let even = [];
+    let count = 0;
+    
+    for (num of arr) {
+        if (num % 2 === 0) count++;
+    }
+
+    // return even.length;
+    return count;
+}
+
+console.log(countEven(arr));
+
+// Simple guard condition
+function printName(name) {
+    if (name === '' || name === undefined) {
+        console.log('No name provided')
+    } else {
+        console.log(name);
+    }
+}
+
+printName('Math');
+printName();
+printName('');
+
+// Small pattern
+let star = '*'
+for (let i = 0; i < 3; i++) {
+    console.log(star);
+    star += '*';
+}
